27527f01d9538ec5928c866cf269e8f3acddd3d5
Jarl Holta <Jarlholta@gmail.com>
2017-05-25 03:08:25 +0200

    Adjustments + SendMessage method.

--- DC_methods.pas
+++ DC_methods.pas
@@ -1,28 +1,36 @@
 {$include_once WinTypes.pas}
 //--------------------------------------------------------------------------------------------------------------------\\
+//-----| Messages and Message Queues  >  Message Reference  >  Message Functions  |-----------------------------------||
+//--------------------------------------------------------------------------------------------------------------------//
+
+// LRESULT WINAPI SendMessage( _In_ HWND   hWnd, _In_ UINT Msg, _In_ WPARAM wParam, _In_ LPARAM lParam);
+function LibUser32.SendMessage(hwnd: HWND; Msg: UInt32; wPar: WPARAM; lPar: LPARAM): LRESULT; static; external 'SendMessageW@User32.dll' + WINAPI_CC;
+
+
+//--------------------------------------------------------------------------------------------------------------------\\
 //-----| Device Contexts  >  Device Context Reference  >  Device Context Functions  |---------------------------------||
 //--------------------------------------------------------------------------------------------------------------------//
 
-function LibGdi32.CancelDC(dc: HDC): BOOL; static; external 'CancelDC@gdi32.dll stdcall';
-//function LibGdi32.CreateDC(lpszDriver: LPCTSTR; lpszDevice:LPCTSTR; lpszOutput: LPCTSTR; const lpInitData: ^DEVMODE): HDC; static; external 'CreateDC@gdi32.dll stdcall'; 
-function LibGdi32.DeleteDC(dc: HDC): BOOL; static; external 'DeleteDC@gdi32.dll stdcall';
-function LibGdi32.DeleteObject(hObject: HGDIOBJ): BOOL; static; external 'DeleteObject@gdi32.dll stdcall';
-function LibGdi32.GetCurrentObject(dc: HDC; uObjectType:UInt32): HGDIOBJ; static; external 'GetCurrentObject@gdi32.dll stdcall';
-//function LibGdi32.ResetDC(hdc: HDC; const lpInitData: ^DEVMODE): HDC; static; external 'ResetDC@gdi32.dll stdcall';
-function LibGdi32.RestoreDC(hdc: HDC; nSavedDC: Int32): BOOL; static; external 'RestoreDC@gdi32.dll stdcall';
-function LibGdi32.SaveDC(dc: HDC): Int32; static; external 'SaveDC@gdi32.dll stdcall';
-function LibGdi32.GetObject(hObject: HGDIOBJ; cbBuffer: Int32; lpvObject: Pointer): Int32; static; external 'GetObjectW@gdi32.dll stdcall';
-function LibGdi32.CreateCompatibleDC(dc: HDC): HDC; static; external 'CreateCompatibleDC@gdi32.dll stdcall';
-function LibGdi32.SelectObject(dc:HDC; hObject: HGDIOBJ): HGDIOBJ; static; external 'SelectObject@gdi32.dll stdcall';
+function LibGdi32.CancelDC(dc: HDC): BOOL; static; external 'CancelDC@gdi32.dll' + WINAPI_CC;
+//function LibGdi32.CreateDC(lpszDriver: LPCTSTR; lpszDevice:LPCTSTR; lpszOutput: LPCTSTR; const lpInitData: ^DEVMODE): HDC; static; external 'CreateDC@gdi32.dll' + WINAPI_CC; 
+function LibGdi32.DeleteDC(dc: HDC): BOOL; static; external 'DeleteDC@gdi32.dll' + WINAPI_CC;
+function LibGdi32.DeleteObject(hObject: HGDIOBJ): BOOL; static; external 'DeleteObject@gdi32.dll' + WINAPI_CC;
+function LibGdi32.GetCurrentObject(dc: HDC; uObjectType:UInt32): HGDIOBJ; static; external 'GetCurrentObject@gdi32.dll' + WINAPI_CC;
+//function LibGdi32.ResetDC(hdc: HDC; const lpInitData: ^DEVMODE): HDC; static; external 'ResetDC@gdi32.dll' + WINAPI_CC;
+function LibGdi32.RestoreDC(hdc: HDC; nSavedDC: Int32): BOOL; static; external 'RestoreDC@gdi32.dll' + WINAPI_CC;
+function LibGdi32.SaveDC(dc: HDC): Int32; static; external 'SaveDC@gdi32.dll' + WINAPI_CC;
+function LibGdi32.GetObject(hObject: HGDIOBJ; cbBuffer: Int32; lpvObject: Pointer): Int32; static; external 'GetObjectW@gdi32.dll' + WINAPI_CC;
+function LibGdi32.CreateCompatibleDC(dc: HDC): HDC; static; external 'CreateCompatibleDC@gdi32.dll' + WINAPI_CC;
+function LibGdi32.SelectObject(dc:HDC; hObject: HGDIOBJ): HGDIOBJ; static; external 'SelectObject@gdi32.dll' + WINAPI_CC;
 
 
-function LibUser32.GetDC(wnd: HWND): HDC; static; external 'GetDC@user32.dll stdcall';
-function LibUser32.GetWindowDC(wnd: HWND): HDC; static; external 'GetWindowDC@user32.dll stdcall';
-function LibUser32.GetDCEx(wnd: HWND; hrgnClip:HRGN; flags:DWord): HDC; static; external 'GetDCEx@user32.dll stdcall';
-function LibUser32.ReleaseDC(wnd: HWND; hdc: HDC): Int32; static; external 'ReleaseDC@user32.dll stdcall';
+function LibUser32.GetDC(wnd: HWND): HDC; static; external 'GetDC@user32.dll' + WINAPI_CC;
+function LibUser32.GetWindowDC(wnd: HWND): HDC; static; external 'GetWindowDC@user32.dll' + WINAPI_CC;
+function LibUser32.GetDCEx(wnd: HWND; hrgnClip:HRGN; flags:DWord): HDC; static; external 'GetDCEx@user32.dll' + WINAPI_CC;
+function LibUser32.ReleaseDC(wnd: HWND; hdc: HDC): Int32; static; external 'ReleaseDC@user32.dll' + WINAPI_CC;
 
 
-function LibGdi32.GetDCBrushColor(dc: HDC): COLORREF; static; external 'GetDCBrushColor@gdi32.dll stdcall';
-function LibGdi32.GetDCPenColor(dc: HDC): COLORREF; static; external 'GetDCPenColor@gdi32.dll stdcall';
-function LibGdi32.SetDCBrushColor(dc: HDC; crColor:COLORREF): COLORREF; static; external 'SetDCBrushColor@gdi32.dll stdcall';
-function LibGdi32.SetDCPenColor(dc: HDC; crColor:COLORREF): COLORREF; static; external 'SetDCPenColor@gdi32.dll stdcall';
+function LibGdi32.GetDCBrushColor(dc: HDC): COLORREF; static; external 'GetDCBrushColor@gdi32.dll' + WINAPI_CC;
+function LibGdi32.GetDCPenColor(dc: HDC): COLORREF; static; external 'GetDCPenColor@gdi32.dll' + WINAPI_CC;
+function LibGdi32.SetDCBrushColor(dc: HDC; crColor:COLORREF): COLORREF; static; external 'SetDCBrushColor@gdi32.dll' + WINAPI_CC;
+function LibGdi32.SetDCPenColor(dc: HDC; crColor:COLORREF): COLORREF; static; external 'SetDCPenColor@gdi32.dll' + WINAPI_CC;
--- sysinfo_methods.pas
+++ sysinfo_methods.pas
@@ -4,7 +4,7 @@
 //--------------------------------------------------------------------------------------------------------------------//
 
 // BOOL WINAPI GetComputerName( _Out_LPTSTR lpBuffer, _Inout_LPDWORD lpnSize); [@Kernel32.dll]
-function LibKernel32.GetComputerName(lpBuffer: LPWSTR; var nSize: DWord): BOOL; static; external 'GetComputerNameW@Kernel32.dll stdcall';
+function LibKernel32.GetComputerName(lpBuffer: LPWSTR; var nSize: DWord): BOOL; static; external 'GetComputerNameW@Kernel32.dll' + WINAPI_CC;
 function LibKernel32.GetComputerName(): WideString; static; overload;
 var size:DWord = 1024;
 begin
@@ -14,13 +14,13 @@ begin
 end;
 
 // void WINAPI GetNativeSystemInfo( _Out_LPSYSTEM_INFO lpSystemInfo); [@Kernel32.dll]
-procedure LibKernel32.GetNativeSystemInfo(out SystemInfo: SYSTEM_INFO); static; external 'GetNativeSystemInfo@Kernel32.dll stdcall';
+procedure LibKernel32.GetNativeSystemInfo(out SystemInfo: SYSTEM_INFO); static; external 'GetNativeSystemInfo@Kernel32.dll' + WINAPI_CC;
 
 // BOOL WINAPI GetProductInfo( _In_DWORD dwOSMajorVersion, _In_DWORD dwOSMinorVersion, _In_DWORD dwSpMajorVersion, _In_DWORD dwSpMinorVersion, _Out_PDWORD pdwReturnedProductType); [@Kernel32.dll]
-function LibKernel32.GetProductInfo(dwOSMajorVersion: DWord; dwOSMinorVersion: DWord; dwSpMajorVersion: DWord; dwSpMinorVersion: DWord; out ReturnedProductType: DWord): BOOL; static; external 'GetProductInfo@Kernel32.dll stdcall';
+function LibKernel32.GetProductInfo(dwOSMajorVersion: DWord; dwOSMinorVersion: DWord; dwSpMajorVersion: DWord; dwSpMinorVersion: DWord; out ReturnedProductType: DWord): BOOL; static; external 'GetProductInfo@Kernel32.dll' + WINAPI_CC;
 
 // UINT WINAPI GetSystemDirectory( _Out_LPTSTR lpBuffer, _In_UINT uSize); [@Kernel32.dll]
-function LibKernel32.GetSystemDirectory(lpBuffer: LPWSTR; uSize: UInt32): UInt32; static; external 'GetSystemDirectoryW@Kernel32.dll stdcall';
+function LibKernel32.GetSystemDirectory(lpBuffer: LPWSTR; uSize: UInt32): UInt32; static; external 'GetSystemDirectoryW@Kernel32.dll' + WINAPI_CC;
 function LibKernel32.GetSystemDirectory(): WideString; static; overload;
 var size:UInt32 = 1024;
 begin
@@ -29,13 +29,13 @@ begin
 end;
 
 // void WINAPI GetSystemInfo( _Out_LPSYSTEM_INFO lpSystemInfo); [@Kernel32.dll]
-procedure LibKernel32.GetSystemInfo(out SystemInfo: SYSTEM_INFO); static; external 'GetSystemInfo@Kernel32.dll stdcall';
+procedure LibKernel32.GetSystemInfo(out SystemInfo: SYSTEM_INFO); static; external 'GetSystemInfo@Kernel32.dll' + WINAPI_CC;
 
 // BOOL WINAPI GetSystemRegistryQuota( _Out_opt_PDWORD pdwQuotaAllowed, _Out_opt_PDWORD pdwQuotaUsed); [@Kernel32.dll]
-function LibKernel32.GetSystemRegistryQuota(out dwQuotaAllowed: DWord; out dwQuotaUsed: DWord): BOOL; static; external 'GetSystemRegistryQuota@Kernel32.dll stdcall';
+function LibKernel32.GetSystemRegistryQuota(out dwQuotaAllowed: DWord; out dwQuotaUsed: DWord): BOOL; static; external 'GetSystemRegistryQuota@Kernel32.dll' + WINAPI_CC;
 
 // UINT WINAPI GetSystemWindowsDirectory( _Out_LPTSTR lpBuffer, _In_UINT uSize); [@Kernel32.dll]
-function LibKernel32.GetSystemWindowsDirectory(lpBuffer: LPWSTR; uSize: UInt32): UInt32; static; external 'GetSystemWindowsDirectoryW@Kernel32.dll stdcall';
+function LibKernel32.GetSystemWindowsDirectory(lpBuffer: LPWSTR; uSize: UInt32): UInt32; static; external 'GetSystemWindowsDirectoryW@Kernel32.dll' + WINAPI_CC;
 function LibKernel32.GetSystemWindowsDirectory(): WideString; static; overload;
 var size:UInt32 = 1024;
 begin
@@ -44,7 +44,7 @@ begin
 end;
 
 // UINT WINAPI GetSystemWow64Directory( _Out_LPTSTR lpBuffer, _In_UINT uSize); [@Kernel32.dll]
-function LibKernel32.GetSystemWow64Directory(lpBuffer: LPWSTR; uSize: UInt32): UInt32; static; external 'GetSystemWow64DirectoryW@Kernel32.dll stdcall';
+function LibKernel32.GetSystemWow64Directory(lpBuffer: LPWSTR; uSize: UInt32): UInt32; static; external 'GetSystemWow64DirectoryW@Kernel32.dll' + WINAPI_CC;
 function LibKernel32.GetSystemWow64Directory(): WideString; static; overload;
 var size:UInt32 = 1024;
 begin
@@ -53,7 +53,7 @@ begin
 end;
 
 // BOOL WINAPI GetUserName( _Out_LPTSTR lpBuffer, _Inout_LPDWORD lpnSize); [@Advapi32.dll]
-function LibAdvapi32.GetUserName(lpBuffer: LPWSTR; out nSize: DWord): BOOL; static; external 'GetUserNameW@Advapi32.dll stdcall';
+function LibAdvapi32.GetUserName(lpBuffer: LPWSTR; out nSize: DWord): BOOL; static; external 'GetUserNameW@Advapi32.dll' + WINAPI_CC;
 function LibAdvapi32.GetUserName(): WideString; static; overload;
 var size:DWord = 1024;
 begin
@@ -64,10 +64,10 @@ end;
 
 
 // DWORD WINAPI GetVersion(); [@Kernel32.dll]
-function LibKernel32.GetVersion(): DWord; static; external 'GetVersion@Kernel32.dll stdcall';
+function LibKernel32.GetVersion(): DWord; static; external 'GetVersion@Kernel32.dll' + WINAPI_CC;
 
 // UINT WINAPI GetWindowsDirectory( _Out_LPTSTR lpBuffer, _In_UINT uSize); [@Kernel32.dll]
-function LibKernel32.GetWindowsDirectory(lpBuffer: LPWSTR; uSize: UInt32): UInt32; static; external 'GetWindowsDirectoryW@Kernel32.dll stdcall';
+function LibKernel32.GetWindowsDirectory(lpBuffer: LPWSTR; uSize: UInt32): UInt32; static; external 'GetWindowsDirectoryW@Kernel32.dll' + WINAPI_CC;
 function LibKernel32.GetWindowsDirectory(): WideString; static; overload;
 var size:UInt32 = 1024;
 begin
@@ -76,11 +76,11 @@ begin
 end;
 
 // BOOL WINAPI IsProcessorFeaturePresent( _In_DWORD ProcessorFeature); [@Kernel32.dll]
-function LibKernel32.IsProcessorFeaturePresent(ProcessorFeature: DWord): BOOL; static; external 'IsProcessorFeaturePresent@Kernel32.dll stdcall';
+function LibKernel32.IsProcessorFeaturePresent(ProcessorFeature: DWord): BOOL; static; external 'IsProcessorFeaturePresent@Kernel32.dll' + WINAPI_CC;
 
 // BOOL WINAPI QueryPerformanceCounter( _Out_LARGE_INTEGER *lpPerformanceCount); [@Kernel32.dll]
-function LibKernel32.QueryPerformanceCounter(var PerformanceCount: UInt64): BOOL; static; external 'QueryPerformanceCounter@Kernel32.dll stdcall';
+function LibKernel32.QueryPerformanceCounter(var PerformanceCount: UInt64): BOOL; static; external 'QueryPerformanceCounter@Kernel32.dll' + WINAPI_CC;
 
 // BOOL WINAPI QueryPerformanceFrequency( _Out_LARGE_INTEGER *lpFrequency); [@Kernel32.dll]
-function LibKernel32.QueryPerformanceFrequency(var Frequency: UInt64): BOOL; static; external 'QueryPerformanceFrequency@Kernel32.dll stdcall';
+function LibKernel32.QueryPerformanceFrequency(var Frequency: UInt64): BOOL; static; external 'QueryPerformanceFrequency@Kernel32.dll' + WINAPI_CC;
 
